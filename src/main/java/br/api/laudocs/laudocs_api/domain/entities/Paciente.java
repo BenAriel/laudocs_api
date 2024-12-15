4a23786ecabd294ba6e12a5e10c04ba8a1d3d984
Lucas <lugab037@gmail.com>
2024-12-15 18:55:48 -0300

    chore: Update patient data.

--- src/main/java/br/api/laudocs/laudocs_api/domain/entities/Paciente.java
+++ src/main/java/br/api/laudocs/laudocs_api/domain/entities/Paciente.java
@@ -2,34 +2,37 @@ package br.api.laudocs.laudocs_api.domain.entities;
 
 import java.time.LocalDate;
 
-import jakarta.annotation.Nonnull;
-import jakarta.persistence.Column;
+import br.api.laudocs.laudocs_api.api.dto.PacienteDTO;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "tb_pacientes")
+@NoArgsConstructor
+@AllArgsConstructor
 public class Paciente {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Column(unique = true)
-    private String CPF;
-
-    @Nonnull
+    private String cpf;
     private String nome;
-
-    
     private LocalDate dataNasc;
-
     private int idade;
+
+    public Paciente(PacienteDTO dto) {
+        this.cpf = dto.getCpf();
+        this.nome = dto.getNome();
+        this.dataNasc = dto.getDataNasc();
+        this.idade = dto.getIdade();
+    }
 }
