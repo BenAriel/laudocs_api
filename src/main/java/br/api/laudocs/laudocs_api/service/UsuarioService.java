29c03577647b2f9420e0386ab4d49916a7779739
Lucas <lugab037@gmail.com>
2024-12-15 18:55:26 -0300

    refactor: Move checkVazio function to ValidationUtils class.

--- src/main/java/br/api/laudocs/laudocs_api/service/UsuarioService.java
+++ src/main/java/br/api/laudocs/laudocs_api/service/UsuarioService.java
@@ -11,6 +11,7 @@ import br.api.laudocs.laudocs_api.api.dto.UsuarioDTO;
 import br.api.laudocs.laudocs_api.domain.entities.Usuario;
 import br.api.laudocs.laudocs_api.domain.repository.UsuarioRepository;
 import br.api.laudocs.laudocs_api.exception.ValidationException;
+import br.api.laudocs.laudocs_api.exception.ValidationUtils;
 
 @Service
 public class UsuarioService {
@@ -32,8 +33,8 @@ public class UsuarioService {
     }
 
     public UsuarioDTO createUser(UsuarioCreateDTO usuarioCreateDTO) {
-        checkVazio(usuarioCreateDTO.getNome(), "Nome não pode ser vazio.");
-        checkVazio(usuarioCreateDTO.getSenha(), "Senha não pode ser vazia.");
+        ValidationUtils.checkVazio(usuarioCreateDTO.getNome(), "Nome não pode ser vazio.");
+        ValidationUtils.checkVazio(usuarioCreateDTO.getSenha(), "Senha não pode ser vazia.");
 
         if (getEmailExistId(usuarioCreateDTO.getEmail()) != null)
             throw new ValidationException("Email já cadastrado no sistema.");
@@ -48,8 +49,8 @@ public class UsuarioService {
         if (!op.isPresent())
             throw new ValidationException("Usuário não existe.");
 
-        checkVazio(usuarioDTO.getNome(), "Novo nome não pode ser vazio.");
-        checkVazio(usuarioDTO.getEmail(), "Novo email não pode ser vazio.");
+        ValidationUtils.checkVazio(usuarioDTO.getNome(), "Novo nome não pode ser vazio.");
+        ValidationUtils.checkVazio(usuarioDTO.getEmail(), "Novo email não pode ser vazio.");
 
         Long idEmail = getEmailExistId(usuarioDTO.getEmail());
         if (idEmail != null && idEmail != usuarioDTO.getId())
@@ -70,11 +71,6 @@ public class UsuarioService {
             throw new ValidationException("Usuário não existe.");
 
         repo.deleteById(userId);
-    }
-
-    public void checkVazio(String nome, String exception) {
-        if (nome == null || nome.equals(""))
-            throw new ValidationException(exception);
     }
 
     public Long getEmailExistId(String email) {
