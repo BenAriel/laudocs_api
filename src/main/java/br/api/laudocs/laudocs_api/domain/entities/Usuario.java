31d5b8866dd52a742e0ab765cb4796be41c8493d
Lucas <lugab037@gmail.com>
2024-12-15 17:16:20 -0300

    Update: Add encryptPassword function to user entity.

--- src/main/java/br/api/laudocs/laudocs_api/domain/entities/Usuario.java
+++ src/main/java/br/api/laudocs/laudocs_api/domain/entities/Usuario.java
@@ -1,32 +1,47 @@
 package br.api.laudocs.laudocs_api.domain.entities;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import br.api.laudocs.laudocs_api.api.dto.UsuarioCreateDTO;
+import br.api.laudocs.laudocs_api.api.dto.UsuarioDTO;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 
 @Getter
 @Setter
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "tb_usuarios")
 public class Usuario {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
-
-
     private String nome;
-
-
     private String senha;
-
-
     private String email;
 
+    public Usuario(UsuarioDTO dto) {
+        this.nome = dto.getNome();
+        this.email = dto.getEmail();
+    }
+
+    public Usuario(UsuarioCreateDTO dto) {
+        this.nome = dto.getNome();
+        this.email = dto.getEmail();
+        this.senha = encodePassword(dto.getSenha());
+    }
     
+    private String encodePassword(String password) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        return passwordEncoder.encode(password);
+    }
 }
