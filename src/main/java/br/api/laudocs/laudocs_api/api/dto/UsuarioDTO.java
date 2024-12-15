39c88a9c6c2b2a6ac3e7706201a0f725cd6fa697
Lucas <lugab037@gmail.com>
2024-12-15 17:15:35 -0300

    chore: Create UsuarioDTO for user data transfer.

--- src/main/java/br/api/laudocs/laudocs_api/api/dto/UsuarioDTO.java
+++ src/main/java/br/api/laudocs/laudocs_api/api/dto/UsuarioDTO.java
@@ -1,5 +1,29 @@
 package br.api.laudocs.laudocs_api.api.dto;
 
+import br.api.laudocs.laudocs_api.domain.entities.Usuario;
+import jakarta.annotation.Nonnull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UsuarioDTO {
-    
+    private Long id;
+    private String nome;
+    @Nonnull
+    private String email;
+
+    public UsuarioDTO(Usuario user) {
+        this.id = user.getId();
+        this.nome = user.getNome();
+        this.email = user.getEmail();
+    }
+
+    public static UsuarioDTO toDTO(Usuario user) {
+        return new UsuarioDTO(user.getId(), user.getNome(), user.getEmail());
+    }
 }
