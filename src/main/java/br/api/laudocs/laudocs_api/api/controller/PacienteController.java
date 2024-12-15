322b7c3b8ef5c1d4f799ad29320c4596878d8ffa
Lucas <lugab037@gmail.com>
2024-12-15 18:51:41 -0300

    feat: create patient controller.

---
+++ src/main/java/br/api/laudocs/laudocs_api/api/controller/PacienteController.java
@@ -0,0 +1,49 @@
+package br.api.laudocs.laudocs_api.api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.api.laudocs.laudocs_api.api.dto.PacienteDTO;
+import br.api.laudocs.laudocs_api.service.PacienteService;
+
+
+@RestController
+@RequestMapping("api/v1/paciente")
+public class PacienteController {
+    @Autowired
+    PacienteService serv;
+
+    @GetMapping
+    public ResponseEntity<?> getAllPacientes() {
+        return ResponseEntity.ok(serv.getAllPacientes());
+    }
+
+    @GetMapping("/{pacienteId}")
+    public ResponseEntity<?> getPaciente(@PathVariable Long pacienteId) {
+        return ResponseEntity.ok(serv.getPaciente(pacienteId));
+    }
+
+    @PostMapping("/criar")
+    public ResponseEntity<?> createPaciente(@RequestBody PacienteDTO pacienteDTO) {
+        return ResponseEntity.ok(serv.createPaciente(pacienteDTO));
+    }
+
+    @PutMapping("/alterar")
+    public ResponseEntity<?> updatePaciente(@RequestBody PacienteDTO pacienteDTO) {
+        return ResponseEntity.ok(serv.updatePaciente(pacienteDTO));
+    }
+
+    @DeleteMapping("/remover/{pacienteId}")
+    public ResponseEntity<?> removePaciente(@PathVariable Long pacienteId) {
+        serv.removePaciente(pacienteId);
+        return ResponseEntity.ok("Paciente removido.");
+    }
+}
