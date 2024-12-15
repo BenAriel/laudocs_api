0965390cb4e69940da2c475e864c64c038c8c94a
Lucas <lugab037@gmail.com>
2024-12-15 18:51:49 -0300

    feat: create patient dto.

--- src/main/java/br/api/laudocs/laudocs_api/api/dto/PacienteDTO.java
+++ src/main/java/br/api/laudocs/laudocs_api/api/dto/PacienteDTO.java
@@ -1,5 +1,33 @@
 package br.api.laudocs.laudocs_api.api.dto;
 
+import java.time.LocalDate;
+
+import br.api.laudocs.laudocs_api.domain.entities.Paciente;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class PacienteDTO {
-    
+    private Long id;
+    private String cpf;
+    private String nome;
+    private LocalDate dataNasc;
+    private int idade;
+
+    public PacienteDTO(Paciente paciente) {
+        this.id = paciente.getId();
+        this.cpf = paciente.getCpf();
+        this.nome = paciente.getNome();
+        this.dataNasc = paciente.getDataNasc();
+        this.idade = paciente.getIdade();
+    }
+
+    public static PacienteDTO toDTO(Paciente paciente) {
+        return new PacienteDTO(paciente.getId(), paciente.getCpf(), paciente.getNome(), paciente.getDataNasc(), paciente.getIdade());
+    }
 }
