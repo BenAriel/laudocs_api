6300179d19882b51f560dd1f0fd9df67c0d37e03
Lucas <lugab037@gmail.com>
2024-12-15 17:12:20 -0300

    feat: create user controller.

---
+++ src/main/java/br/api/laudocs/laudocs_api/api/controller/UsuarioController.java
@@ -0,0 +1,56 @@
+package br.api.laudocs.laudocs_api.api.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.api.laudocs.laudocs_api.api.dto.UsuarioCreateDTO;
+import br.api.laudocs.laudocs_api.api.dto.UsuarioDTO;
+import br.api.laudocs.laudocs_api.service.UsuarioService;
+
+@RestController
+@RequestMapping("api/v1/usuario")
+@CrossOrigin
+public class UsuarioController {
+    @Autowired
+    UsuarioService serv;
+    
+    @GetMapping
+    public ResponseEntity<?> getAllUsers() {
+        return ResponseEntity.ok(serv.getAllUsers());
+    }
+    
+    @GetMapping("/{userId}")
+    public ResponseEntity<?> getUser(@PathVariable Long userId) {
+        UsuarioDTO user = serv.getUser(userId);
+        return ResponseEntity.ok(user);
+    }
+
+    @PostMapping("/criar")
+    public ResponseEntity<?> createUser(@Valid @RequestBody UsuarioCreateDTO usuarioCreateDTO) {
+        UsuarioDTO user = serv.createUser(usuarioCreateDTO);
+        return ResponseEntity.ok(user);
+    }
+
+    @PutMapping("/alterar")
+    public ResponseEntity<?> updateUser(@Valid @RequestBody UsuarioDTO usuarioDTO) {
+        UsuarioDTO user = serv.updateUser(usuarioDTO);
+        return ResponseEntity.ok(user);
+    }
+
+    @DeleteMapping("/remover/{userId}")
+    public ResponseEntity<?> removeUser(@PathVariable Long userId) {
+        serv.remove(userId);
+        return ResponseEntity.ok("Usuário removido");
+    }
+}
