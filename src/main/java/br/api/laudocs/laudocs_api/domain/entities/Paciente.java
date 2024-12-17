73f59deef691e16dca9ac8602e36df7d69e0d3e5
CaioAndersonMM <caioandersonmartins@gmail.com>
2024-12-17 19:43:55 -0300

    feat: Adicionado relacionamento OneToMany entre Paciente e Laudo

--- src/main/java/br/api/laudocs/laudocs_api/domain/entities/Paciente.java
+++ src/main/java/br/api/laudocs/laudocs_api/domain/entities/Paciente.java
@@ -1,12 +1,14 @@
 package br.api.laudocs.laudocs_api.domain.entities;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import br.api.laudocs.laudocs_api.api.dto.PacienteDTO;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -28,6 +30,9 @@ public class Paciente {
     private String nome;
     private LocalDate dataNasc;
     private int idade;
+
+    @OneToMany(mappedBy = "paciente")
+    private List<Laudo> laudos;
 
     public Paciente(PacienteDTO dto) {
         this.cpf = dto.getCpf();
