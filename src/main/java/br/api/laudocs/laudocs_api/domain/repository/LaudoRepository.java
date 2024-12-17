69870c7214618dd9536e143a4578ce17300c9ebc
CaioAndersonMM <caioandersonmartins@gmail.com>
2024-12-17 19:44:52 -0300

    feat: Adicionado JPA extends e implementação de LaudoRepository com métodos para buscar Laudos por pacienteId e consultaId

--- src/main/java/br/api/laudocs/laudocs_api/domain/repository/LaudoRepository.java
+++ src/main/java/br/api/laudocs/laudocs_api/domain/repository/LaudoRepository.java
@@ -1,5 +1,12 @@
 package br.api.laudocs.laudocs_api.domain.repository;
 
-public interface LaudoRepository {
-    
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.api.laudocs.laudocs_api.domain.entities.Laudo;
+
+public interface LaudoRepository  extends JpaRepository<Laudo, Long> {
+    List<Laudo> findByPacienteId(Long pacienteId);
+    List<Laudo> findByConsultaId(Long consultaId);
 }
